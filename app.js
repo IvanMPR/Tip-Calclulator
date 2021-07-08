2466c69b5116e45d6a9417efe5e845a42084246b
Ivan MPR <ivsanmas@gmail.com>
2021-07-08 18:30:28 +0200

    Added reset button, alert for illegal inputs, auto focus

--- app.js
+++ app.js
@@ -1,48 +1,66 @@
-const billInput = document.getElementById("bill").value;
-const splitInput = document.getElementById("split").value;
-const tipInput = document.getElementById("tip").value;
-const animationBox = document.getElementById("display-info");
-const tipDisplayContent = document.getElementById("tip-display");
-const totalDisplayContent = document.getElementById("total-display");
-const personDisplayContent = document.getElementById("per-person-display");
+const bill = document.getElementById("bill");
+const tip = document.getElementById("tip");
+const split = document.getElementById("split");
+const inputs = document.querySelectorAll("input");
+///////////////////////////////////////////////////////////////
+const calculatedTip = document.getElementById("tip-display");
+const calculatedTotal = document.getElementById("total-display");
+const calculatedSplit = document.getElementById("per-person-display");
+///////////////////////////////////////////////////////////////
 const animation = document.getElementById("animation");
-
-document.getElementById("btn").addEventListener("click", function () {
-  //   var delay = Math.floor(Math.random() * 2 + 1) * 1000;
-  if (!billInput && !tipInput)
-    alert("Please enter bill amount and tipping percentage amount !");
-  if (!billInput) alert("Please type in the bill amount !");
-  if (!tipInput) alert("Please type in your tipping percentage !");
-
-  const calculateTip = function (bill, split = 1, tipPercentage) {
-    const tipDisplay = (bill * tipPercentage) / 100;
-    const totalDisplay = bill + tipDisplay;
-    const perPersonDisplay = totalDisplay / split;
-
-    tipDisplayContent.innerText = tipDisplay;
-    totalDisplayContent.innerText = totalDisplay;
-    personDisplayContent.innerText = perPersonDisplay;
-  };
-
-  animationBox.classList.remove("hidden");
-  animationBox.classList.add("visible");
-
-  //   setTimeout(() => {
-  //   var billInput = document.getElementById("bill");
-  //   var bill = parseInt(billInput.value);
-  //   var splitInput = document.getElementById("split");
-  //   var split = parseInt(splitInput.value);
-  //   var tipInput = document.getElementById("tip");
-  //   var tip = parseInt(tipInput.value);
-  //   var tipDisplay = (bill * tip) / 100;
-  //   var totalDisplay = bill + tipDisplay;
-  //   var perPersonDisplay = totalDisplay / split;
-  //   document.getElementById("animation").classList.remove("visible");
-  //   document.getElementById("animation").classList.add("hidden");
-  //   document.getElementById("tip-display").innerHTML = tipDisplay;
-  //   document.getElementById("total-display").innerHTML = totalDisplay;
-  //   document.getElementById("per-person-display").innerHTML = perPersonDisplay;
-  document.getElementById("display-info").classList.remove("hidden");
-  document.getElementById("display-info").classList.add("visible");
-  //   }, delay);
+const button = document.querySelector("#calc");
+const resetButton = document.querySelector("#reset");
+const resultsBox = document.getElementById("display-info");
+//////////////////////////////////////////////////////////////////////
+window.addEventListener("load", function () {
+  bill.focus();
 });
+
+//////////////////////////////
+function calcTip(billAm, splitAm = 1, tipAm) {
+  if (!billAm || !tipAm) {
+    alert(
+      "Bill amount and tip amount are mandatory fields ! Please fill them both !"
+    );
+    inputs.forEach((input) => (input.value = ""));
+    bill.focus();
+
+    return;
+  }
+
+  calculatedTip.textContent = ((billAm * tipAm) / 100).toFixed(2);
+  calculatedTotal.textContent = (billAm + (billAm * tipAm) / 100).toFixed(2);
+  calculatedSplit.textContent = (
+    (billAm + (billAm * tipAm) / 100) /
+    splitAm
+  ).toFixed(2);
+
+  resultsBox.classList.remove("hidden");
+  resultsBox.classList.add("visible");
+
+  // return console.log(billAm, splitAm, tipAm);
+}
+
+function reset() {
+  resultsBox.classList.remove("visible");
+  resultsBox.classList.add("hidden");
+  // ////////////////////////////////
+  calculatedTip.textContent = "";
+  calculatedTotal.textContent = "";
+  calculatedSplit.textContent = "";
+  ////////////////////////////////////
+  inputs.forEach((input) => (input.value = ""));
+  /////////////////////////////////////////////////
+  bill.focus();
+}
+
+// // console.log(billInput.value);
+// // console.log(button);
+button.addEventListener("click", function () {
+  calcTip(
+    parseInt(bill.value),
+    parseInt(split.value) || undefined,
+    parseInt(tip.value)
+  );
+});
+resetButton.addEventListener("click", reset);
